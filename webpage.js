0d98a562e4c333c93bbe99d02b5081167bf30394
orangegameube <156160264+orangegameube@users.noreply.github.com>
2024-07-17 15:42:36 -0500

    Update webpage.js

--- webpage.js
+++ webpage.js
@@ -1,8 +1,27 @@
-var trace1 = {
+var traceOG = {
   x: ['DV', 'Blue (470)', 'Cyan (500)', 'Green (550)', 'Yellow (600)', 'Orange (630)', 'Red (665)', 'Deep Red (780)', 'IR1', 'IR2', 'IR3', 'IR4'],
   y: [20.8, 13.23, 14.68, 14.65, 9.62, 8.72, 17.93, 21.92, 18.33, 52.06, 7.99, 93.08],
   type: 'bar',
 }
+
+var data = [traceOG];
+var layout = {title: 'Spectrometer Experiment Wavelength Vs. % Reflectivity',
+              xaxis: {title: 'Color and Wavelength (nm)'},
+              yaxis: {title: '% Reflectivity'}
+             };
+
+
+Plotly.newPlot('myDiv', data, layout);
+
+
+
+
+// var trace1 = {
+//   x: [95, 844, 1009, 1050, 999, 1126, 948, 893, 902, 871, 883, 818],
+//   y: [20.8, 13.23, 14.68, 14.65, 9.62, 8.72, 17.93, 21.92, 18.33, 52.06, 7.99, 93.08],
+//   name: 'Sample Paper',
+//   type: 'bar',
+// }
 
 // var trace2 = {
 //   x: [95, 251, 216, 235, 182, 185, 248, 270, 243, 499, 158, 768],
@@ -11,15 +30,12 @@ var trace1 = {
 //   type: 'bar'
 // }
 
+// var layout = {barmode: 'group',
+//              title: 'Spectrometer Experiment Wavelength Vs. % Reflectivity',
+//               xaxis: {title: 'Color and Wavelength (nm)'},
+//               yaxis: {title: '% Reflectivity'}
+//              };
 
-var data = [trace1];
-var layout = {barmode: 'bar',
-             title: 'Spectrometer Experiment Wavelength Vs. % Reflectivity',
-              xaxis: {title: 'Color and Wavelength (nm)'},
-              yaxis: {title: '% Reflectivity'}
-             };
-
-
-Plotly.newPlot('myDiv', data, layout);
-
-Plotly.newPlot('myDiv2', data2, layout2);
+// var data = [trace1, trace2];
+// var layout = {barmode: 'group'};
+// Plotly.newPlot('myDiv2', data2, layout2);
